fcc135c30bfe589b840a724d32c21e78ca2265de
diguardoa <diguardoa@gmail.com>
2017-09-12 11:51:34 +0200

    cleaned

--- start_2.sh
+++
@@ -1,1 +0,0 @@
-sudo qemu-system-x86_64 -m 1024 tinycore4.7.7_apache2_php_2.qcow2 -enable-kvm -netdev tap,id=net0,ifname=tap1,script=no,downscript=no -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:28 
--- tap_create_2.sh
+++
@@ -1,3 +0,0 @@
-sudo ip tuntap add dev tap1 mode tap
-sudo ip link set tap1 up promisc on
-sudo brctl addif virbr0 tap1
